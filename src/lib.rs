ec9fd10df479a773b65d8ef61befe90f309d2256
rigma <rigbuntu@gmail.com>
2020-01-04 21:37:29 +0100

    feat: add all supported platforms

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,18 @@ extern crate libc;
 
 #[macro_use]
 mod macros;
+
+cfg_if! {
+    if #[cfg(all(unix, target_os = "macos"))] {
+        mod darwin;
+        pub use darwin::*;
+    } else if #[cfg(all(unix, not(target_os = "openbsd")))] {
+        mod unix;
+        pub use unix::*;
+    } else if #[cfg(windows)] {
+        mod windows;
+        pub use windows::*;
+    } else {
+        // TODO: add API here with unimplemented macros
+    }
+}
---
+++ src/unix.rs
@@ -0,0 +1,2 @@
+pub const ALIGN_SIZE: i32 = 4096;
+pub const BLOCK_SIZE: i32 = 4096;
