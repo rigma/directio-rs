c6533e7afc8f108082e591b9e862d418aad1dfc4
rigma <rigbuntu@gmail.com>
2020-01-05 18:19:23 +0100

    refactor: add some conditionnal dependency
    fix: removes darwin module

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,12 @@
 #[macro_use]
 extern crate cfg_if;
+#[cfg(unix)]
 extern crate libc;
-
 #[cfg(windows)]
 extern crate winapi;
 
 cfg_if! {
-    if #[cfg(all(unix, target_os = "macos"))] {
-        mod darwin;
-        pub use darwin::*;
-    } else if #[cfg(all(unix, not(target_os = "openbsd")))] {
+    if #[cfg(all(unix, not(target_os = "openbsd")))] {
         mod unix;
         pub use unix::*;
     } else if #[cfg(windows)] {
