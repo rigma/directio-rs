1c4a5480a08fbdd5e833c07802b9276d06e9c26c
rigma <rigbuntu@gmail.com>
2020-01-05 15:04:15 +0100

    fix: applying clippy advices and removing trailing spaces

--- src/unix.rs
+++ src/unix.rs
@@ -52,11 +52,11 @@ impl DirectFileExt for fs::File {
 }
 
 /// Unix-specific DirectIO extensions to [`std::fs::OpenOptions`].
-/// 
+///
 /// [`std::fs::OpenOptions`]: https://doc.rust-lang.org/std/fs/struct.OpenOptions.html
 pub trait DirectOpenOptionsExt {
     /// Add `O_DIRECT` to the `flags` argument of `open`.
-    /// 
+    ///
     /// By providing this flag on UNIX family, you'll be able to access to the
     /// file without passing through kernel buffering. This is slower but may 
     /// be useful when you need some data to be really on the storage disk (eg.
@@ -67,13 +67,12 @@ pub trait DirectOpenOptionsExt {
     /// ```no_run
     /// use std::fs::OpenOptions;
     /// use directio::DirectOpenOptionsExt;
-    /// 
-    /// # fn main() {
+    ///
     /// let mut options = OpenOptions::new();
     /// options.direct();
-    /// 
+    ///
     /// let file = options.open("foo.txt");
-    /// # }
+    ///
     /// ```
     fn direct(&mut self) -> &mut Self;
 }
