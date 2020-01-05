1a679c5403a0fb4f81dd436e1eb72d370ee514d7
rigma <rigbuntu@gmail.com>
2020-01-05 15:02:59 +0100

    feat: add directio file extension for unix

--- src/unix.rs
+++ src/unix.rs
@@ -1,10 +1,55 @@
+use std::path::Path;
 use std::fs;
+use std::io;
 
 /// Size used to align the data buffer
 pub const ALIGN_SIZE: i32 = 4096;
 
 /// Minimum block size
 pub const BLOCK_SIZE: i32 = 4096;
+
+/// Unix-specific DirectIO extensions to [`std::fs::File`].
+///
+/// [`std::fs::file`]: https://doc.rust-lang.org/std/fs/struct.File.html
+pub trait DirectFileExt {
+    /// Open a file in read-only mode with the `O_DIRECT` flag.
+    ///
+    /// See [`DirectOpenOptionsExt::direct`] for more details.
+    ///
+    /// # Errors
+    ///
+    /// This function is returning the same errors of [`std::fs::File::open`].
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use directio::DirectFileExt;
+    /// use std::fs::File;
+    ///
+    /// fn main() -> std::io::Result<()> {
+    ///     let mut f = fs::direct_open("foo.txt")?;
+    ///     Ok(())
+    /// }
+    /// ```
+    ///
+    /// [`DirectOpenOptionsExt::direct`]: trait.DirectFileExt.html#method.direct
+    /// [`std::fs::File::open`]: https://doc.rust-lang.org/std/fs/struct.File.html#method.open
+    fn direct_open<P>(path: P) -> io::Result<fs::File>
+    where
+        P: AsRef<Path>;
+}
+
+impl DirectFileExt for fs::File {
+    fn direct_open<P>(path: P) -> io::Result<fs::File>
+    where
+        P: AsRef<Path>,
+    {
+        fs::OpenOptions::new()
+            .read(true)
+            .direct()
+            .open(path.as_ref())
+    }
+}
 
 /// Unix-specific DirectIO extensions to [`std::fs::OpenOptions`].
 /// 
