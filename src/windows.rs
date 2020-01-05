68c1e0340c2b1208120bb19f2618042567eeb343
rigma <rigbuntu@gmail.com>
2020-01-05 18:17:04 +0100

    feat: add windows calls to enable directio

--- src/windows.rs
+++ src/windows.rs
@@ -1,6 +1,8 @@
-use std::path::Path;
 use std::fs;
 use std::io;
+use std::os::windows::fs::OpenOptionsExt;
+use std::path::Path;
+use winapi::um::winbase::{FILE_FLAG_NO_BUFFERING, FILE_FLAG_WRITE_THROUGH};
 
 /// Size used to align the data buffer
 pub const ALIGN_SIZE: i32 = 4096;
@@ -15,31 +17,43 @@ pub const BLOCK_SIZE: i32 = 4096;
 ///
 /// [`std::fs::file`]: https://doc.rust-lang.org/std/fs/struct.File.html
 pub trait DirectFileExt {
-  /// Open a file in read-only mode with the `O_DIRECT` flag.
-  ///
-  /// See [`DirectOpenOptionsExt::direct`] for more details.
-  ///
-  /// # Errors
-  ///
-  /// This function is returning the same errors of [`std::fs::File::open`].
-  ///
-  /// # Examples
-  ///
-  /// ```no_run
-  /// use directio::DirectFileExt;
-  /// use std::fs::File;
-  ///
-  /// fn main() -> std::io::Result<()> {
-  ///     let mut f = fs::direct_open("foo.txt")?;
-  ///     Ok(())
-  /// }
-  /// ```
-  ///
-  /// [`DirectOpenOptionsExt::direct`]: trait.DirectOpenOptionsExt.html#tymethod.direct
-  /// [`std::fs::File::open`]: https://doc.rust-lang.org/std/fs/struct.File.html#method.open
-  fn direct_open<P>(path: P) -> io::Result<fs::File>
-  where
-      P: AsRef<Path>;
+    /// Open a file in read-only mode with the `O_DIRECT` flag.
+    ///
+    /// See [`DirectOpenOptionsExt::direct`] for more details.
+    ///
+    /// # Errors
+    ///
+    /// This function is returning the same errors of [`std::fs::File::open`].
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use directio::DirectFileExt;
+    /// use std::fs::File;
+    ///
+    /// fn main() -> std::io::Result<()> {
+    ///     let mut f = fs::direct_open("foo.txt")?;
+    ///     Ok(())
+    /// }
+    /// ```
+    ///
+    /// [`DirectOpenOptionsExt::direct`]: trait.DirectOpenOptionsExt.html#tymethod.direct
+    /// [`std::fs::File::open`]: https://doc.rust-lang.org/std/fs/struct.File.html#method.open
+    fn direct_open<P>(path: P) -> io::Result<fs::File>
+    where
+        P: AsRef<Path>;
+}
+
+impl DirectFileExt for fs::File {
+    fn direct_open<P>(path: P) -> io::Result<fs::File>
+    where
+        P: AsRef<Path>,
+    {
+        fs::OpenOptions::new()
+            .read(true)
+            .attributes(FILE_FLAG_NO_BUFFERING | FILE_FLAG_WRITE_THROUGH)
+            .open(path.as_ref())
+    }
 }
 
 /// DirectIO extensions to [`std::fs::OpenOptions`].
@@ -49,24 +63,30 @@ pub trait DirectFileExt {
 ///
 /// [`std::fs::OpenOptions`]: https://doc.rust-lang.org/std/fs/struct.OpenOptions.html
 pub trait DirectOpenOptionsExt {
-  /// Add `O_DIRECT` to the `flags` argument of `open`.
-  ///
-  /// By providing this flag on UNIX family, you'll be able to access to the
-  /// file without passing through kernel buffering. This is slower but may 
-  /// be useful when you need some data to be really on the storage disk (eg.
-  /// for data replication for instance).
-  ///
-  /// # Examples
-  ///
-  /// ```no_run
-  /// use std::fs::OpenOptions;
-  /// use directio::DirectOpenOptionsExt;
-  ///
-  /// let mut options = OpenOptions::new();
-  /// options.direct();
-  ///
-  /// let file = options.open("foo.txt");
-  ///
-  /// ```
-  fn direct(&mut self) -> &mut Self;
+    /// Add `O_DIRECT` to the `flags` argument of `open`.
+    ///
+    /// By providing this flag on UNIX family, you'll be able to access to the
+    /// file without passing through kernel buffering. This is slower but may
+    /// be useful when you need some data to be really on the storage disk (eg.
+    /// for data replication for instance).
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use std::fs::OpenOptions;
+    /// use directio::DirectOpenOptionsExt;
+    ///
+    /// let mut options = OpenOptions::new();
+    /// options.direct();
+    ///
+    /// let file = options.open("foo.txt");
+    ///
+    /// ```
+    fn direct(&mut self) -> &mut Self;
+}
+
+impl DirectOpenOptionsExt for fs::OpenOptions {
+    fn direct(&mut self) -> &mut Self {
+        self.attributes(FILE_FLAG_NO_BUFFERING | FILE_FLAG_WRITE_THROUGH)
+    }
 }
