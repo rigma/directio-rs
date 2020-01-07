66715b0fc3dfb5f29d04816b873c0178468a5aa9
rigma <rigbuntu@gmail.com>
2020-01-07 21:49:56 +0100

    test: add integration tests for direct file

---
+++ tests/direct_file.rs
@@ -0,0 +1,35 @@
+use std::fs::{self, File};
+use std::io::{Read, Write};
+use directio::DirectFileExt;
+
+#[test]
+fn it_should_write_a_file() {
+    let block = [b'a'; 4096];
+    let file = File::direct_create("foo.txt");
+    assert!(file.is_ok());
+
+    let mut file = file.unwrap();
+    assert!(file.write_all(&block).is_ok());
+
+    fs::remove_file("foo.txt").unwrap();
+}
+
+#[test]
+fn it_should_read_a_file() {
+    let expected: [u8; 4096] = [b'a'; 4096];
+    File::direct_create("bar.txt")
+        .unwrap()
+        .write_all(&expected)
+        .unwrap();
+
+    let file = File::direct_open("bar.txt");
+    assert!(file.is_ok());
+
+    let mut file = file.unwrap();
+    let mut block = Vec::new();
+
+    file.read_to_end(&mut block).unwrap();
+    assert_eq!(expected[0..32], block[0..32]);
+
+    fs::remove_file("bar.txt").unwrap();
+}
